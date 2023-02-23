2877e744318c26b51f34228620bbdcd2ebc23be3
MathiasSondergard <sondergard.mathias@gmail.com>
2023-02-23 12:08:37 +0100

    DagligFast relation til Dosis

--- src/ordinationstuderende/ordination/DagligFast.java
+++ src/ordinationstuderende/ordination/DagligFast.java
@@ -1,5 +1,44 @@
 package ordinationstuderende.ordination;
 
-public class DagligFast {
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+
+public class DagligFast extends Ordination {
     // TODO
+    private Dosis[] doser = new Dosis[4];
+
+
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel, Dosis[] doser) {
+        super(startDen, slutDen, laegemiddel);
+        this.doser = doser;
+    }
+
+    public Dosis[] getDoser() {
+        return doser;
+    }
+
+
+    @Override
+    public double samletDosis() {
+        double sum = antalDage() * doegnDosis();
+        return sum;
+    }
+
+    @Override
+    public double doegnDosis() {
+        double sum = 0;
+        for (int i = 0; i < 4; i++) {
+            sum = doser[i].getAntal();
+        }
+        return sum;
+    }
+
+    @Override
+    public String getType() {
+        return "DagligFast";
+    }
+
+
 }
