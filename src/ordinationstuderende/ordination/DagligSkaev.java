6aa1e221208d7cfb0eb87f7e91db40411e4435d4
abd9911 <abd.alsahlii@gmail.com>
2023-02-23 11:01:04 +0100

    Kompositionen mellem DagligSkæv og Dosis realiseret ved en ArrayList<Dosis>

--- src/ordinationstuderende/ordination/DagligSkaev.java
+++ src/ordinationstuderende/ordination/DagligSkaev.java
@@ -1,12 +1,16 @@
 package ordinationstuderende.ordination;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class DagligSkaev {
     // TODO
-
-
+    private final ArrayList<Dosis> doser = new ArrayList<>();
+    public ArrayList<Dosis> getDoser() {
+        return new ArrayList<>(doser);
+    }
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
+        doser.add(new Dosis(tid,antal));
     }
 }
