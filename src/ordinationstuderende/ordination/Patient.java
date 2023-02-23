abd8bca84d6d538265659475e06d4aa62375599e
eaajalk <eaajalk@students.eaaa.dk>
2023-02-23 10:10:18 +0100

    Tilføjet sammenhæng fra patient til ordination

--- src/ordinationstuderende/ordination/Patient.java
+++ src/ordinationstuderende/ordination/Patient.java
@@ -1,9 +1,12 @@
 package ordinationstuderende.ordination;
 
+import java.util.ArrayList;
+
 public class Patient {
     private String cprnr;
     private String navn;
     private double vaegt;
+    private final ArrayList<Ordination> ordinationer = new ArrayList<>();
 
     // TODO: Link til Ordination
 
@@ -31,6 +34,20 @@ public class Patient {
 
     public void setVaegt(double vaegt){
         this.vaegt = vaegt;
+    }
+
+    public ArrayList<Ordination> getOrdination() {
+        return new ArrayList<>(ordinationer);
+    }
+    public void addPerson(Ordination ordination) {
+        if (!ordinationer.contains(ordination)) {
+        ordinationer.add(ordination);
+    }
+    }
+    public void removePerson(Ordination ordination) {
+        if (ordinationer.contains(ordination)) {
+            ordinationer.remove(ordination);
+        }
     }
 
     //TODO: Metoder (med specifikation) til at vedligeholde link til Ordination
