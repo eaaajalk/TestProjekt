818852eefaef498f8b2b5ec522e405b70a5176a8
abd9911 <abd.alsahlii@gmail.com>
2023-02-28 10:59:15 +0100

    dagligskævTest

--- src/ordinationstuderende/ordination/DagligSkaev.java
+++ src/ordinationstuderende/ordination/DagligSkaev.java
@@ -17,7 +17,7 @@ public class DagligSkaev extends Ordination {
     }
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
-        doser.add(new Dosis(tid,antal));
+            doser.add(new Dosis(tid, antal));
     }
 
     @Override
